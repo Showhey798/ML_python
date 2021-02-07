b94ef546457db3d1ad087f29a82297875afcb33c
Showhey <shota.cal0123@gmail.com>
2021-02-07 11:00:04 +0900

    とりあえずの終了

--- MSE.py
+++ MSE.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 #%%
 class MSE():
 
-    def __init__(self, M=3, l2_penalty=0):
-        self.M = 3
-        self.l2_penal = l2_penalty
-
-    def fit(self, X_train, t_train):
+    def __init__(self, X_train, t_train):
         self.x = X_train
         self.t = t_train
+
+    def fit(self, M=3, l2_penalty=0):
+        self.M = 3
+        self.l2_penal = l2_penalty
 
         phi = np.empty((self.x.shape[0],self.M))
 
@@ -18,8 +18,8 @@ class MSE():
             phi[:,j] = self.x**j
         
         theta = np.linalg.solve(phi.T.dot(phi) + self.l2_penal*np.eye(self.M), phi.T.dot(self.t))
+        #theta = np.linalg.solve(phi.T.dot(phi), phi.T.dot(self.t))
         func = lambda x : sum([th * x**i for i,th in zip(range(self.M), theta)])
-
         return theta, func
 
 
@@ -32,7 +32,7 @@ def generate_data(noise_scale, N):
 
 def display_data(func, X, t, y_lim=(-1.4,1.4),name=None,title=None):
     fig = plt.figure(figsize=(7,5))
-    x = np.linspace(X.min(), X.max(), 500)
+    x = np.linspace(X.min(), X.max(), 200)
     plt.ylim(y_lim)
     if title is not None:
         plt.title(title)
@@ -49,13 +49,9 @@ def display_data(func, X, t, y_lim=(-1.4,1.4),name=None,title=None):
 #%%
 
 if __name__ == "__main__":
-    x_train, t_train = generate_data(0.3, 15)
-    x_test, t_test = generate_data(0.3, 15)
+    x_train, t_train = generate_data(0.3, 10)
+    x_test, t_test = generate_data(0.3, 10)
 
-    model = MSE(M=9)
-    theta, func = model.fit(x_train, t_train)
-
+    model = MSE(x_train, t_train)
+    theta, func = model.fit(M=4)
     display_data(func, x_test, t_test)
-
-
-# %%
